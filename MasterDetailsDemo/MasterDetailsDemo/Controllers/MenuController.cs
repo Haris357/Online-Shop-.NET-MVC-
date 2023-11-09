f7094f987a49a750c7dc99d39ff894c8f66008b7
Haris Muhammad Imran <harisimran7857@gmail.com>
2023-11-09 15:10:42 +0500

    Commit

---
+++ MasterDetailsDemo/MasterDetailsDemo/Controllers/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MasterDetailsDemo.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Location()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult Links()
+        {
+            return View();
+        }
+    }
+}
